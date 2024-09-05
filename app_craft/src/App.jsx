037c4d05b9ffc0c356ffa2250f7eeb1adcb43c18
cche0258 <cche0258@student.monash.edu>
2024-09-05 23:44:55 +0800

    Update App and Firebase config for task data retrieval
    - Added Firestore document fetching in App.jsx using useEffect
    - Initialized Firestore in firebaseConfig.js for database access

--- app_craft/src/App.jsx
+++ app_craft/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CancelButton from './components/CancelButton.jsx';
 import CreateTaskButton from './components/CreateTaskButton.jsx';
 import AddTaskOverlay from './components/AddTaskOverlay.jsx';  // Import the overlay component
@@ -6,10 +6,24 @@ import './App.css';
 import NavigationBar from './components/NavigationBar.jsx';
 import CrossButton from './components/CrossButton.jsx';
 import Home from './pages/home.jsx';
+import {doc, getDoc} from 'firebase/firestore';
+import {db} from './firebaseConfig';
+
 
 function App() {
   // State to control overlay visibility
   const [isOverlayVisible, setOverlayVisible] = useState(false);
+
+  const docRef = doc(db, 'tasks', 'Yn7xWRHWqZlKgEiWTo0n')
+
+  const getData = async () => {  
+    const docSnap = await getDoc(docRef);
+
+    console.log(docSnap.data());
+  }
+
+  useEffect(() => {getData()}, [])
+
 
   // Function to handle the "Create" button click
   const handleCreateButtonClick = () => {
--- app_craft/src/firebaseConfig.js
+++ app_craft/src/firebaseConfig.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getFirestore} from '@firebase/firestore';
+import {getFirestore, collection, getDocs} from '@firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,5 +19,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const analytics = getAnalytics(app);
+//const db = getFirestore()
+//const colRef = collection(db, 'tasks')
+
+//getDocs(colRef).then((snapshot) => {console.log(snapshot.docs)})
+export const db = getFirestore(app);
 export const firestore = getFirestore(app);
