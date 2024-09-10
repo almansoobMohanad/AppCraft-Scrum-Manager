0c062c50e3f9994e391eda61dba814fb5bb901ae
A.I.Hunter <Walkercpt@gmail.com>
2024-09-10 11:38:42 +0800

    Added EditFilesInDB.jsx functions to update the tasks in DB

---
+++ app_craft/src/components/EditFilesInDB.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { db } from '../firebase/firebaseConfig.js';
+import { getDocs, collection, doc, setDoc } from 'firebase/firestore';
+
+export function EditFilesInDB(taskID) {
+    const dbRef =  doc(db, 'tasks', taskID);
+
+    const taskTypes = ['Story', 'Bug'];
+    const taskStages = ['Planning', 'Development', 'Testing', 'Integration'];
+    const priorities = ['Low', 'Medium', 'Important', 'Urgent'];
+    const availableTags = ['Frontend', 'Backend', 'API', 'Database', 'Framework', 'Testing', 'UI', 'UX'];
+
+    const changeName = async (newName) => {
+        setDoc(dbRef, { name: newName }, { merge: true });
+    }
+
+    const changeType = async (newType) => {
+        if (newType in taskTypes) {
+            setDoc(dbRef, { type: newType }, { merge: true });
+        } else {
+            alert("Invalid task type");
+            console.error("Invalid task type");
+        }
+    }
+
+    const changeStage = async (newStage) => {
+        if (newStage in taskStages) {
+            setDoc(dbRef, { stage: newStage }, { merge: true });
+        } else {
+            alert("Invalid task stage");
+            console.error("Invalid task stage");
+        }
+    }
+
+    const changeStoryPoints = async (newStoryPoints) => {
+        setDoc(dbRef, { storyPoints: newStoryPoints }, { merge: true });
+    }
+
+    const changePriority = async (newPriority) => {
+        if (newPriority in priorities) {
+            setDoc(dbRef, { priority: newPriority }, { merge: true });
+        } else {
+            alert("Invalid priority");
+            console.error("Invalid priority");
+        }
+    }
+
+    const changeTags = async (newTags) => {
+        setDoc(dbRef, { tags: newTags }, { merge: true });
+    }
+
+    const changeAssignee = async (newAssignee) => {
+        setDoc(dbRef, { assignee: newAssignee }, { merge: true });
+    }
+
+    const changeDescription = async (newDescription) => {
+        setDoc(dbRef, { description: newDescription }, { merge: true });
+    }
+
+    return {
+        changeName,
+        changeType,
+        changeStage,
+        changeStoryPoints,
+        changePriority,
+        changeTags,
+        changeAssignee,
+        changeDescription
+    };
+}
