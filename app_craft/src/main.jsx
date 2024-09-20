959190ffedded9e50830c0bf18e7263efa5b3a85
charmsie0920 <charmainechee0920@gmail.com>
2024-09-21 01:31:51 +0800

    created browser router for navigation bar

--- app_craft/src/main.jsx
+++ app_craft/src/main.jsx
@@ -1,8 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './cssFiles/index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './cssFiles/index.css';
+import SprintBoard from './pages/Board/SprintBoard.jsx';
+import AccountPage from './pages/Account/AccountPage.jsx';
+import AdminView from './pages/Admin/AdminView.jsx';
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />, 
+    },    
+    {
+      path: "/sprintboard",
+      element: <SprintBoard />, 
+    },
+    {
+      path: "/account",
+      element: <AccountPage/>, 
+    },
+    {
+      path: "/admin",
+      element: <AdminView/>, 
+    },
+  ]);
+
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <RouterProvider router={router}/>
 )
