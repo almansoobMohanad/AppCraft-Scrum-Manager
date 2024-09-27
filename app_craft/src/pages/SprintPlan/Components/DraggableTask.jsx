f170210ec1296482c09a48ec7fa199414e566e60
mohanadNeotrix <133473666+mohanadNeotrix@users.noreply.github.com>
2024-09-28 01:01:43 +0800

    Initial Commit DraggableTask -Draggable Task is not a seperate component

---
+++ app_craft/src/pages/SprintPlan/Components/DraggableTask.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Draggable } from 'react-beautiful-dnd';
+import '../css/draggableTask.css';
+
+const DraggableTask = ({ task, index, onClick }) => {
+    return (
+        <Draggable key={task.id} draggableId={task.id} index={index}>
+            {(provided) => (
+                <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    className="task"
+                    onClick={() => onClick(task)}
+                >
+                    <h3 className='task-name-h3'>{task.name}</h3>
+                    <p className={`priority-display ${task.priority.toLowerCase()}`}>{task.priority}</p>
+                    <div className="task-tags">
+                        {task.tags.map(tag => (
+                            <span key={tag} className={`tag-display ${tag.toLowerCase()}`}>{tag}</span>
+                        ))}
+                    </div>
+                    <p className="storypoint-circle">{task.storypoint}</p>
+                </div>
+            )}
+        </Draggable>
+    );
+};
+
+export default DraggableTask;
