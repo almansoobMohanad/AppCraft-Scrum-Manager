856ccc012e593e74e6448846946107cb49651392
mteh0004 <mteh0004@student.monash.edu>
2024-10-08 02:56:43 +0800

    US: delete account - Files updated -- AccountTable.jsx --- added delete button -- AccountTable.css --- css colour update

--- app_craft/src/pages/Admin/component/AccountTable.jsx
+++ app_craft/src/pages/Admin/component/AccountTable.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import '../css/AccountTable.css'; // Create and import a CSS file for styling if needed
 
-function AccountTable({ title, accounts }) {
+function AccountTable({ title, accounts, onDelete }) {
     return (
         <div className="account-table">
             <h2>{title}</h2>
@@ -10,12 +10,16 @@ function AccountTable({ title, accounts }) {
                 <thead>
                     <tr>
                         <th>Username</th>
+                        <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     {accounts.map((account, index) => (
                         <tr key={index}>
                             <td>{account.username}</td>
+                            <td>
+                                <button onClick={() => onDelete(account.id)}>Delete</button>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
@@ -28,10 +32,12 @@ AccountTable.propTypes = {
     title: PropTypes.string.isRequired,
     accounts: PropTypes.arrayOf(
         PropTypes.shape({
+            id: PropTypes.string.isRequired,
             username: PropTypes.string.isRequired,
             isAdmin: PropTypes.bool.isRequired,
         })
     ).isRequired,
+    onDelete: PropTypes.func.isRequired,
 };
 
 export default AccountTable;
